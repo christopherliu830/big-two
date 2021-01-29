7eef622900735fd5fb0fba5f0191e985b0451ed8
Christopher Liu <christopherliu830@gmail.com>
2021-01-29 17:56:32 -0500

    colorpicker that does nothing

--- packages/frontend/src/components/ColorPicker.tsx
+++ packages/frontend/src/components/ColorPicker.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { ToggleButton, ToggleButtonGroup } from '@material-ui/lab';
+import { ToggleButton, ToggleButtonGroup, ToggleButtonProps } from '@material-ui/lab';
 import { makeStyles } from '@material-ui/core';
 
 const colors = [
-  '#363e18',
-  '#422a10',
-  '#381c2f',
-  '#181931',
-  '#163331',
+  '#2d5b6b',
+  '#c47a53',
+  '#8f4731',
+  '#52494c',
+  '#7b7d2a',
 ]
 
 const useStyles = makeStyles({
@@ -18,49 +18,66 @@ const useStyles = makeStyles({
     justifyContent: 'space-around',
     marginTop: '0.5em',
     marginBottom: '0.5em',
-  }
+  },
+  button: {
+    flexGrow: 1,
+    '&:hover': {
+      boxShadow: '0 0 1px 2px black',
+      zIndex: 10,
+    },
+  },
+  selected: {
+    boxShadow: '0 0 1px 2px black',
+    zIndex: 10,
+  },
 })
+
+interface StylizedButtonProps extends ToggleButtonProps {
+  color: string;
+}
 
 export const ColorPicker = () => {
   const classes = useStyles();
+  const [ color, setColor ] = React.useState(colors[0]);
+
+  const handleChange = (event: React.MouseEvent<HTMLElement>, value: string | null) => {
+    console.log(value);
+    setColor(value);
+  }
+
+  const createButton = (color:string) => {
+    return (
+      <ToggleButton 
+        value={color}
+        style={{ 
+          backgroundColor: color, 
+        }}
+        classes={{
+          root: classes.button,
+          selected: classes.selected
+        }}
+      >
+        <div style={{
+          width: '100%',
+          height: '100%',
+        }} />
+      </ToggleButton>
+    )
+  };
 
   return (
-    <ToggleButtonGroup className={classes.group}>
-      <ToggleButton value="0" style={{ backgroundColor: colors[0] }}>
-        <div style={{
-          backgroundColor: colors[0],
-          width: '100%',
-          height: '100%',
-        }} />
-      </ToggleButton>
-      <ToggleButton value="1" style={{ backgroundColor: colors[1] }}>
-        <div style={{
-          backgroundColor: colors[0],
-          width: '100%',
-          height: '100%',
-        }} />
-      </ToggleButton>
-      <ToggleButton value="2" style={{ backgroundColor: colors[2] }}>
-        <div style={{
-          backgroundColor: colors[0],
-          width: '100%',
-          height: '100%',
-        }} />
-      </ToggleButton>
-      <ToggleButton value="3" style={{ backgroundColor: colors[3] }}>
-        <div style={{
-          backgroundColor: colors[0],
-          width: '100%',
-          height: '100%',
-        }} />
-      </ToggleButton>
-      <ToggleButton value="4" style={{ backgroundColor: colors[4] }}>
-        <div style={{
-          backgroundColor: colors[0],
-          width: '100%',
-          height: '100%',
-        }} />
-      </ToggleButton>
+    <ToggleButtonGroup 
+      className={classes.group} 
+      value={color} 
+      exclusive
+      onChange={handleChange}
+      size="large"
+    >
+      { createButton(colors[0]) }
+      { createButton(colors[1]) }
+      { createButton(colors[2]) }
+      { createButton(colors[3]) }
+      { createButton(colors[4]) }
     </ToggleButtonGroup>
   )
 
