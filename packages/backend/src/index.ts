6be906c423b1a201abfc9b0d43e9d52e185d9112
Christopher Liu <christopherliu830@gmail.com>
2021-02-08 19:22:36 -0500

    rewrote require to esm

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,19 +1,21 @@
-import { Socket } from 'socket.io';
-import { Table } from './manager';
-import { NetworkMessage as Message } from 'common';
-import { Request, Response } from 'express';
+import { Socket } from "socket.io";
+import { Table } from "./manager";
+import { NetworkMessage as Message } from "common";
+import { Request, Response } from "express";
+import * as cors from "cors";
+import * as express from "express";
+import * as HTTP from "http";
+import { Server } from "socket.io";
 
-const cors = require('cors');
-const express = require('express');
 const app = express();
-const http = require('http').createServer(app);
+const http = HTTP.createServer(app);
 
-const io = require('socket.io')(http, {
+const io = new Server(http, {
   cors: {
     origin: true,
     preflightContinue: true,
     credentials: true,
-    methods: ['GET', 'POST'],
+    methods: ["GET", "POST"],
   },
 });
 
@@ -22,14 +24,13 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 const tables: Record<string, Table> = {};
-app.post('/create-room', (req: Request, res: Response) => {
-  const { winCondition, rounds, hidden } = req.body;
-  console.log(winCondition, rounds, hidden );
-  const tableId = (Math.random().toString(36) + '00000000').slice(2, 10);
-  tables[tableId] = new Table(io, tableId);
-  res.status(200).send({table: tableId});
-})
-http.listen(3000, () => console.log('listening on', 3000));
+app.post("/create-room", (req: Request, res: Response) => {
+  const { winCondition, rounds, hidden, owner } = req.body;
+  const tableId = (Math.random().toString(36) + "00000000").slice(2, 10);
+  tables[tableId] = new Table(io, tableId, owner);
+  res.status(200).send({ table: tableId });
+});
+http.listen(3000, () => console.log("listening on", 3000));
 
 io.on(Message.Type.Connect, (socket: Socket) => {
   // Disable socket.send
@@ -42,4 +43,3 @@ io.on(Message.Type.Connect, (socket: Socket) => {
     tables[id]?.join(socket, payload);
   });
 });
-
